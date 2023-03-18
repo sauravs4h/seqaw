1bfce284a91366a8991598c4d34252f55fb61a2d
sauravs4h <sauravs4h@gmail.com>
2023-03-18 23:27:04 +0530

    h

--- index.js
+++ index.js
@@ -4,9 +4,9 @@ const express=require("express");
 const app=express();
 app.use(express.json());
 
- const {sequelize}=require("./config/db")
- const {Users}=require("./models/user.model")
- const {userroute}=require("./routes/user.route")
+//  const {sequelize}=require("./config/db")
+//  const {Users}=require("./models/user.model")
+//  const {userroute}=require("./routes/user.route")
 
 
 
@@ -17,13 +17,13 @@ app.get("/",(req,res)=>{
 app.use("/user",userroute)
 
 app.listen(8000,async()=>{
-    try{
-        await sequelize.authenticate();
-        await sequelize.sync()
-        console.log("connected to db")
-    }
-    catch(err){
-        console.log(err)
-    }
+    // try{
+    //     await sequelize.authenticate();
+    //     await sequelize.sync()
+    //     console.log("connected to db")
+    // }
+    // catch(err){
+    //     console.log(err)
+    // }
     console.log("server is running")
 })
